5d1ec7cb02cb45cb44385ac71277e992ec2404eb
Daniel Condly <onyxmatrix24@gmail.com>
2024-04-10 14:24:59 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by DJC on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
